d56bc2576674b87247ee2e64c0622750da58e7af
pddoze <peter.doze@gmail.com>
2017-07-17 12:03:48 -0400

    Fixing the Code
    Put environmental variables in .bashrc
    so now I can run this subprocess script to run bpz from within python

--- unixScripts/enmass_bpz.py
+++ unixScripts/enmass_bpz.py
@@ -4,18 +4,22 @@ import subprocess
 
 #Use bpz, a photonometric estimation program, en mass for multiple fields at a
 #time
-os.environ["HOME"] = "/home/doze"
-os.environ["BPZPATH"] = "$HOME/bpz-1.99.3"
-os.environ["PYTHONPATH"] = "$PYTHONPATH:$BPZPATH"
-#os.environ[alias] bpz="python $BPZPATH/bpz.py"
-os.environ["NUMERIX"] = "numpy"
-
-#bpz psG048_specz.cat -ZMAX 1.8 -VERBOSE no -INTERP 2 -DZ 0.01 -SPECTRA CWWSB_Benitez2003.list -PRIOR full -PROBS_LITE psG048_specz.probs
-
-#python $BPZPATH/bpzfinalize.py psG055_specz
-
-#os.system('python $BPZPATH/bpz.py psG108_specz.color -ZMAX 1.8 -VERBOSE no -INTERP 2 -DZ 0.01 -SPECTRA CWWSB_Benitez2003.list -PRIOR full -PROBS_LITE psG108_specz.probs')
 
 
-subprocess.Popen(["python", "$BPZPATH/bpz.py", "psG048_specz.color", "-ZMAX", "1.8", "-VERBOSE", "no", "-INTERP", "2", "-DZ", "0.01", "-SPECTRA", "CWWSB_Benitez2003.list", "-PRIOR", "full", "-PROBS_LITE", "psG048_specz.probs"])
+def bpzRun(x):
+    for filename in os.listdir(x):
+        fhalf, shalf = filename.rsplit('.',1)
+        if filename.endswith(".cat"):
+            subprocess.Popen(["python3", "/home/doze/bpz_1.99.3_py3/bpz.py", 
+                              filename, "-ZMAX", "1.8", 
+                              "-VERBOSE", "no", "-INTERP", "2", "-DZ", 
+                              "0.01", "-SPECTRA", "CWWSB_Benitez2003.list", 
+                              "-PRIOR", "full", "-PROBS_LITE", 
+                              fhalf+".probs"])            
+        else:
+            continue
+        
+    
 
+dirpath = "../bpz_1.99.3_py3/test/"
+bpzRun(dirpath)
